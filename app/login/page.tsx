9f0047c180b7411758be3b98d48c40766d97c480
Gustavo A. Roberto <44234235+gustavoaroberto@users.noreply.github.com>
2024-05-31 22:46:39 -0300

    feat: initial project + login screen

---
+++ app/login/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default function Login() {
+  return (
+    <main className={styles.login}>
+      <div className={styles.logo}></div>
+      <form className="box">
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control">
+            <input
+              className="input"
+              type="email"
+              placeholder="e.g. alex@example.com"
+            />
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className="input" type="password" placeholder="********" />
+          </div>
+        </div>
+
+        <button className="button is-primary">Sign in</button>
+      </form>
+    </main>
+  );
+}
